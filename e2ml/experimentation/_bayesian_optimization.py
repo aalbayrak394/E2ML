c080d800421e5f4d843984887fbbbb2b4bbd2051
Aleyna Albayrak <uk076901@student.uni-kassel.de>
2023-07-26 18:50:17 +0200

    evaluation of models on whole dataset

--- e2ml/experimentation/_bayesian_optimization.py
+++ e2ml/experimentation/_bayesian_optimization.py
@@ -143,41 +143,27 @@ def perform_bayesian_optimization(X_cand, gpr, acquisition_func, obj_func, n_eva
     )
 
     # Perform Bayesian optimization until `n_evals` have been performed.
-    X_cand_is_acquired = np.zeros(len(X_cand), dtype=bool)
+    # BEGIN SOLUTION
+    is_acquired = np.zeros(len(X_cand), dtype=bool)
     X_acquired = []
     y_acquired = []
-
-    # n_random_init
-    random_selected_idx = np.random.RandomState(1).choice(len(X_cand), size=n_random_init)
-    X_cand_is_acquired[random_selected_idx] = True
-    [X_acquired.append(X_cand[idx_]) for idx_ in random_selected_idx]
-    [y_acquired.append(obj_func(X_cand[idx_])) for idx_ in random_selected_idx]
-
-    # n_evals
-    for i in range(n_random_init, n_evals):
-        # fit gpr
-        gpr.fit(X_acquired, y_acquired)
-
-        # predict
-        mu, sigma = gpr.predict(X_cand[~X_cand_is_acquired], return_std=True)
-
-        # compute tau
-        tau = np.max(y_acquired)
-
-        # evaluate acquisition function
-        if acquisition_func == 'pi':
-            scores = acquisition_pi(mu, sigma, tau)
-        elif acquisition_func == 'ei':
-            scores = acquisition_ei(mu, sigma, tau)
-        elif acquisition_func == 'ucb':
-            scores = acquisition_ucb(mu, sigma, kappa=1)
-
-        # find candidates according to acq func score
-        acq_func_selected_idx = np.argmax(scores)
-
-        # selection, append list
-        X_cand_is_acquired[acq_func_selected_idx] = True
-        X_acquired.append(X_cand[acq_func_selected_idx])
-        y_acquired.append(obj_func(X_cand[acq_func_selected_idx]))
-
+    for i in range(n_evals):
+        if i < n_random_init:
+            selected_idx = np.random.choice(np.arange(len(X_cand)), size=1)[0]
+        else:
+            gpr.fit(X_acquired, y_acquired)
+            mu, sigma = gpr.predict(X_cand[~is_acquired], return_std=True)
+            tau = np.max(y_acquired)
+            if acquisition_func == 'pi':
+                scores = acquisition_pi(mu, sigma, tau)
+            elif acquisition_func == 'ei':
+                scores = acquisition_ei(mu, sigma, tau)
+            elif acquisition_func == 'ucb':
+                scores = acquisition_ucb(mu, sigma, kappa=1.)
+            selected_idx = np.argmax(scores)
+            not_acquired_indices = np.argwhere(is_acquired == 0).ravel()
+            selected_idx = not_acquired_indices[selected_idx]
+        is_acquired[selected_idx] = True
+        X_acquired.append(X_cand[selected_idx])
+        y_acquired.append(obj_func(X_cand[selected_idx])[0])
     return np.array(X_acquired), np.array(y_acquired)
